138213735d2044e137d1f1ff4bb9ba71f4092083
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2023-09-21 18:09:26 -0300

    continuacao

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,9 +1,17 @@
-export function getWeekDays() {
+interface getWeekDaysParams {
+	short?: boolean
+}
+
+export function getWeekDays({ short = false }:getWeekDaysParams = {}) {
 	const formatter = new Intl.DateTimeFormat("pt-BR", { weekday: "long" });
 
 	return Array.from(Array(7).keys())
 		.map(day => formatter.format(new Date(Date.UTC(2021, 5, day))))
 		.map(weekday => {
+			if (short) {
+				return weekday.substring(0, 3).toUpperCase();
+			}
+		
 			return weekday.charAt(0).toUpperCase().concat(weekday.substring(1));
 		});
 }
